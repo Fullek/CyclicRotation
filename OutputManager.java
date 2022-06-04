77d33bda5123b7d2a61739c9281af14ebb0e1fd0
Fullek <62626590+Fullek@users.noreply.github.com>
2022-06-04 21:40:35 +0200

    Create OutputManager.java

---
+++ OutputManager.java
@@ -0,0 +1,12 @@
+public class OutputManager {
+
+    void getFilledTable(int[] table) {
+
+        for (int number : table) {
+            System.out.print(number + " ");
+        }
+
+        System.out.println();
+    }
+
+}

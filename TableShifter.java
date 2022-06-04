688a5cd511ecc6f9e2d41fd79b8a044329c56c86
Fullek <62626590+Fullek@users.noreply.github.com>
2022-06-04 21:42:51 +0200

    Create TableShifter.java

---
+++ TableShifter.java
@@ -0,0 +1,41 @@
+public class TableShifter {
+
+    TableBuilder tableBuilder;
+    private int n;
+
+    int[] shiftedTable;
+
+    public TableShifter(TableBuilder tableBuilder) {
+        this.tableBuilder = tableBuilder;
+        this.shiftedTable = tableBuilder.getTable();
+    }
+
+    void getShiftingBody() {
+        getNumberOfShifts();
+        shiftTable(n);
+        printTableAfterShift();
+    }
+
+    private void getNumberOfShifts() {
+        System.out.println("How many times to shift array numbers to the right?");
+        n = tableBuilder.inputManager.getInput();
+    }
+
+    private void shiftTable(int n) {
+
+        for (int i = 0; i < n; i++) {
+            int j;
+            int last = shiftedTable[shiftedTable.length-1];
+            for (j = shiftedTable.length-1; j > 0; j--) {
+                shiftedTable[j] = shiftedTable[j-1];
+            }
+            shiftedTable[0] = last;
+        }
+    }
+
+    private void printTableAfterShift() {
+        System.out.print("An array after shifting: ");
+        tableBuilder.outputManager.getFilledTable(shiftedTable);
+    }
+
+}
